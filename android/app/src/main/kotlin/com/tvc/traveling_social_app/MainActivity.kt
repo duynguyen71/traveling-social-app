070b18abafe6432b0d6fe986aecf118f991aacbb
khanhduy86 <khanhduy.dev.bt@gmail.com>
2022-04-18 11:28:12 +0700

    init project

---
+++ android/app/src/main/kotlin/com/tvc/traveling_social_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tvc.traveling_social_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      child: const Text("Login Screen"),
+    );
+  }
+}
