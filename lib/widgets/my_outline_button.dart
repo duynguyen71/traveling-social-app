a6aa81e13ece10843337ccc4bffcce16c499f138
khanhduy86 <khanhduy.dev.bt@gmail.com>
2022-05-27 19:11:27 +0700

    no message

---
+++ lib/widgets/my_outline_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../constants/app_theme_constants.dart';
+import '../utilities/application_utility.dart';
+
+class MyOutlineButton extends StatelessWidget {
+  const MyOutlineButton(
+      {Key? key,
+      required this.onClick,
+      required this.text,
+      this.textColor,
+      this.color,
+      this.borderColor})
+      : super(key: key);
+
+  final Function onClick;
+  final String text;
+  final Color? textColor, color, borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          style: BorderStyle.solid,
+          color:borderColor?? kPrimaryColor,
+        ),
+        borderRadius: BorderRadius.circular(40),
+        color: color??Colors.transparent
+        ),
+      child: TextButton(
+        onPressed: () {
+          onClick();
+        },
+        child: Text(
+          text,
+          style: TextStyle(
+            color: textColor??kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        style: TextButton.styleFrom(
+          elevation: 0,
+        ),
+      ),
+    );
+  }
+}
